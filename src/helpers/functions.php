701ff2148a7c302160037469cc99360569264be5
Osayawe Ogbemudia Terry <terrymarcy2000@yahoo.com>
2018-10-14 19:23:04 +0200

    deprecated guestFileExtension, getFileTmpLocation, and getObjectName functions

--- src/helpers/functions.php
+++ src/helpers/functions.php
@@ -5,6 +5,10 @@ function basePath()
     return realpath(__DIR__ . '/../../');
 }
 
+/**
+ * @deprecated
+ * @return mixed
+ */
 function guestFileExtension()
 {
     $key = array_keys($_FILES)[0];
@@ -12,14 +16,22 @@ function guestFileExtension()
     return pathinfo($filename, PATHINFO_EXTENSION);
 }
 
+/**
+ * @deprecated
+ * @return mixed
+ */
 function getFileTmpLocation()
 {
     $key = array_keys($_FILES)[0];
     return $_FILES[$key]['tmp_name'];
 }
 
+/**
+ * @deprecated
+ * @return mixed
+ */
 function getObjectName($object_key)
 {
     $keys = explode('/', $object_key);
-    return $keys[1] !== "" ? basename($object_key) :"";
+    return $keys[1] !== "" ? basename($object_key) : "";
 }
